2a98594a9aa776500ce8b167ae7585be540d8eb0
Dong. Nguyen Tien - CMC Global DU11 <ntdong2@cmcglobal.vn>
2021-12-02 15:49:02 +0700

    init project

---
+++ src/test/java/com/jenkins/jenkins/docker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkins.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
